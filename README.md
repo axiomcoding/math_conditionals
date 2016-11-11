bbbdfb88b16406e678c026565a0e0dfa1fad857d
github username <github email address>
2016-11-11 15:47:05 -0700

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# math_conditionals
+Follow the commented instructions in `math_conditionals.rb` to pass each RSpec test in `math_conditionals_spec.rb`. 
---
+++ math_conditionals.rb
@@ -0,0 +1,27 @@
+# define a method, how_big, than takes 1 argument, num
+# return "That's a small number." if the input is less than 100, otherwise return "That's a large number."
+
+
+
+
+
+# define a method, even?, than takes 1 argument, num
+# return true if the input is even, otherwise return false
+
+
+
+
+
+
+# define a method, odd?, than takes 1 argument, num
+# return true if the input is odd, otherwise return false
+
+
+
+
+
+
+# define a method, zero?, than takes 1 argument, num
+# return true if the input is 0, otherwise return false
+
+
